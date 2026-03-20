e19b746a0a446797565193fa1eff3b663080552b
mattiads92-png <mattiads92@gmail.com>
2026-03-20 15:26:33 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,9 @@
-self.addEventListener('fetch', function(event) {
-  // Service Worker minimo necessario per far felice Android e farti uscire il banner "Aggiungi a Home"
+// Un Service Worker basilare per bypassare il controllo di installabilità
+self.addEventListener('install', (e) => {
+    self.skipWaiting();
+});
+
+self.addEventListener('fetch', (e) => {
+    // Lasciamo che la rete gestisca tutto normalmente
+    e.respondWith(fetch(e.request));
 });
